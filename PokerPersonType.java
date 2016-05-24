8e77af5d22bb6381d109d0b278e4c46b6378d695
John Long <johnlonggately@gmail.com>
2016-05-24 23:44:25 +0100

    Another file for the program

---
+++ PokerPersonType.java
@@ -0,0 +1,18 @@
+package cardpractice;
+
+public abstract class PokerPersonType {
+	
+	private String name;
+	
+	public PokerPersonType(String name){
+		this.name=name;
+		
+	}
+
+	
+
+	public String getName(){
+		return name;
+	}
+	
+}
